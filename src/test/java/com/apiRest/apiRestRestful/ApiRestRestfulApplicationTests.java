43aa9203c210040b815d2b94d3b84c3dcc22f495
Christakis <banzachristakis@gmail.com>
2024-12-17 17:28:13 -0300

    Iniciando projeto

---
+++ src/test/java/com/apiRest/apiRestRestful/ApiRestRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiRest.apiRestRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
