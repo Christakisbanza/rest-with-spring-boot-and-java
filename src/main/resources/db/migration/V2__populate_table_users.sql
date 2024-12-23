852388bf6b0cbadc7edd27c3eaf14b9670a43e98
Christakis <banzachristakis@gmail.com>
2024-12-23 16:53:33 -0300

    Add migration com flyway

---
+++ src/main/resources/db/migration/V2__populate_table_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, name, email, cpf ) VALUES
+    (1, 'Naturo', 'Naruto@gamil.com', '12354897465'),
+    (2, 'Sasuke', 'Sasuke@gmail.com', '45123698741'),
+    (3, 'Gon', 'Gon@gmail.com', '54898745100');
